e6b7e119b942c0b32140b5f1d877ba3c8cfd5e3f
Stephen P. Schaefer <sschaefer@acm.org>
2015-08-23 20:36:56 -0400

    Reading and writing node headline and text; capturing ^i in anticipation of inserting a new node.

--- site/src/elements/element_outline.erl
+++ site/src/elements/element_outline.erl
@@ -7,18 +7,20 @@
 -export([
     reflect/0,
     render_element/1,
-    event/1
+    event/1,
+    api_event/3
 ]).
 
 %% Done: update textarea with node text
 %% Done: attach node read to focus event
 %% Done: indicate no children with bullet of "O"
-%% Next: node write on blur event
+%% Done: uncollapse handled by javascript
+%% Done: collapse handled by javascript
+%% Done: node write title on blur event
+%% Done: node write text on blur event
 %% Next: node insert
 %% Next: node delete
 %% Next: right click menu on title
-%% Next: collapse handled by javascript
-%% Next: uncollapse handled by javascript
 %% Next: scroll bar for outline
 %% Next: scroll bar for text
 %% Next: set size for outline to be screen (not more)
@@ -31,29 +33,36 @@ reflect() -> record_info(fields, outline).
 % the gn_id is {Module, Function, Argument}, such that
 % {Title, Text, ChildList} = Module:Function(Argument).
 -spec render_element(#outline{}) -> body().
-render_element(#outline{ gn_id=GNID }) ->
-    % nitrogen seems to have trouble with bitstrings; convert
-    { Module, Outline, Arg } = GNID,
-    ?PRINT({Module, Outline, Arg}),
+render_element(#outline{ gn_id={Module, Read, Write, Arg} }) ->
+%    ?PRINT({Module, Read, Write, Arg}),
     UnitID = wf:temp_id(),
     ArrowID = wf:temp_id(),
     TitleID = wf:temp_id(),
     wf:wire(TitleID, TitleID, #event {
     	type = click,
     	delegate = ?MODULE,
-    	postback = { click, {title, UnitID, TitleID, {Module, Outline, Arg} } }
+    	postback = { click, {title, UnitID, TitleID, {Module, Read, Write, Arg} } }
     }),
     wf:wire(TitleID, TitleID, #event {
 	type = focus,
 	delegate = ?MODULE,
-	postback = { focus, {title, UnitID, TitleID, {Module, Outline, Arg} } }
+	postback = { focus, {title, UnitID, TitleID, {Module, Read, Write, Arg} } }
     }),
+    { Title, _, ChildList } = Module:Read(Arg),
     wf:wire(TitleID, TitleID, #event {
 	type = blur,
 	delegate = ?MODULE,
-	postback = { blur, {title, UnitID, TitleID, {Module, Outline, Arg} } }
+	postback = {blur_title, TitleID, {Module, Read, Write, Arg}}
     }),
-    { Title, _, ChildList } = Module:Outline(Arg),
+    wf:wire(TitleID, TitleID, #event {
+	type = keypress,
+	actions = [
+	    #script { script=wf:f("if (event.ctrlKey && ([105].indexOf(event.which) >= 0)) {
+		event.preventDefault();
+		event.stopPropagation();
+		page.control_key('~p', '~p', event.which); };", [TitleID, Arg]) }
+	]
+    }),
     [
 	#panel{ id=UnitID, body= [
 	    #span{ id=ArrowID, body = [#button{ text=
@@ -64,7 +73,7 @@ render_element(#outline{ gn_id=GNID }) ->
 			wf:wire(ArrowID, ArrowID, #event {
 			    type = click,
 			    delegate = ?MODULE,
-			    postback = { click, {expand, UnitID, ArrowID, {Module, Outline, Arg} } }
+			    postback = { click, {expand, UnitID, ArrowID, {Module, Read, Write, Arg} } }
 			}),
 			">"
 		end
@@ -73,51 +82,54 @@ render_element(#outline{ gn_id=GNID }) ->
 	]}
     ].
 
-event({click, {expand, UnitID, ArrowID, GNID}}) ->
-    { Module, Outline, Arg } = GNID,
-    ?PRINT({arrow, UnitID, ArrowID, { Module, Outline, Arg }}),
-    NewArrowID = wf:temp_id(),
-    wf:replace(ArrowID, #span{ id=NewArrowID, body=[#button{ text="V" }] }),
-    {_, _,  ChildList } = Module:Outline(Arg),
+event({click, {expand, UnitID, ArrowID, { Module, Read, Write, Arg }}}) ->
+%    ?PRINT({arrow, UnitID, ArrowID, { Module, Read, Write, Arg }}),
+    ExpandedArrowID = wf:temp_id(),
+    ContractedArrowID = wf:temp_id(),
+    wf:replace(ArrowID, [
+	#span{ id=ExpandedArrowID,   body=[#button{ text="V" }] },
+	#span{ id=ContractedArrowID, body=[#button{ text=">" }], style="display: none" } ]
+    ),
+    {_, _,  ChildList } = Module:Read(Arg),
     Body = [
-	#outline{ gn_id={Module, Outline, C} } || C <- ChildList ],
+	#outline{ gn_id={Module, Read, Write, C} } || C <- ChildList ],
     case ChildList of
 	[] -> ok;
 	_ ->
 	    ChildrenID = wf:temp_id(),
-	    wf:insert_bottom(UnitID, #panel{ style="margin-left: 32px", id=ChildrenID, body=Body}),
-	    wf:wire(NewArrowID, NewArrowID, #event {
+	    Event = #event {
 		type = click,
-		delegate = ?MODULE,
-		postback = { click, {collapse, UnitID, NewArrowID, ChildrenID} }
-	    })
+		actions = [
+		    #toggle{ target=ExpandedArrowID },
+		    #toggle{ target=ContractedArrowID },
+		    #toggle{ target=ChildrenID, options=[{duration, 800}] }
+		]
+	    },
+	    wf:insert_bottom(UnitID, #panel{ style="margin-left: 32px", id=ChildrenID, body=Body}),
+	    wf:wire(ExpandedArrowID, ExpandedArrowID, Event),
+	    wf:wire(ContractedArrowID, ContractedArrowID, Event)
     end;
 
-event({click, {collapse, UnitID, ArrowID, ChildrenID}}) ->
-    wf:update(ArrowID, #button{ text=">" }),
-    wf:wire(ArrowID, ChildrenID, #hide{}),
-    wf:wire(ArrowID, ArrowID, #event {
-    	type = click,
-    	delegate = ?MODULE,
-    	postback = { click, {uncollapse, UnitID, ArrowID, ChildrenID} }
-    });
-
-event({click, {uncollapse, UnitID, ArrowID, ChildrenID}}) ->
-    NewArrowID = wf:temp_id(),
-    wf:replace(ArrowID, #span{ id=NewArrowID, body=[#button{ text="V" }] }),
-    wf:wire(NewArrowID, ChildrenID, #show{}),
-    wf:wire(NewArrowID, NewArrowID, #event {
-	type = click,
-	delegate = ?MODULE,
-	postback = {click, {collapse, UnitID, NewArrowID, ChildrenID} }
-    });
-
-event({focus, {title, UnitID, TitleID, GNID}}) ->
-    { Module, Outline, Arg } = GNID,
-    ?PRINT({focus, {title, UnitID, TitleID, {Module, Outline, Arg}}}),
-    { Title, Text, _ } = Module:Outline(Arg),
+event({focus, {title, _UnitID, _TitleID, { Module, Read, _Write, Arg }}}) ->
+% It would be better to use browser "canonical" values for Title and Text;
+% if it becomes a problem, set up a browser side hash from Arg to { title: , text: }
+    { Title, Text, _ } = Module:Read(Arg),
     wf:set(".gn_"++Arg, Title),
+    wf:set(".current_node", Arg),
     wf:set(".textarea", Text);
 
+event({blur_title, TitleID, {Module, Read, Write, Arg}}) ->
+    {OldTitle, Text, ChildList} = Module:Read(Arg),
+    Title = wf:q(TitleID),
+    case Title of
+	OldTitle -> ok;
+	_ ->
+	    Module:Write(Arg, Title, Text, ChildList)
+    end;
+    
 event(Any) ->
     ?PRINT({Any}).
+
+api_event(control_key, _Tag, [_TitleID, _GraphNodeKey, KeyCode]) ->
+    Msg = wf:f("Control-~p pressed",[KeyCode]),
+    wf:flash(Msg).
--- site/src/index.erl
+++ site/src/index.erl
@@ -8,59 +8,91 @@
 
 main() -> #template { file="./site/templates/bare.html" }.
 
-title() -> "Hello from vw.erl!".
+title() -> "VisiWeave".
 
 body() -> 
     #graph_node{ children = Roots } = visiweave_node_server:read_roots(),
-    OutlineBody = [ #outline{ gn_id={index,read_node,binary_to_list(Node)} } || Node <- Roots ],
+    OutlineBody = [ #outline{ gn_id={index,read_node,write_node,binary_to_list(Node)} } || Node <- Roots ],
     [FirstNode|_] = Roots,
     #graph_node{ text = Text } = visiweave_node_server:read_node(FirstNode),
-    wf:wire(wf:f("jQuery('.resizable').resizable({handles: \"e, s, se\"})")),
-    #panel{
-	style="margin-left:50px;margin-right:50px;margin-top:0px;",
-	body=
-	#table{
-	    style="height:100%;width:100%",
-	    rows=[
-		#tablerow{
-		    cells=[
-			#tablecell{
-			    class="resizable",
-			    style="border:1px solid;vertical-align:top",
-			    body=
-			    #panel{
-				style="margin-top:0px",
-				body=OutlineBody
+    wf:wire(wf:f("jQuery('.resizable').resizable({handles: \"e\"})")),
+    wf:wire(textarea, textarea, #event{ type=blur, postback=blur_text }),
+    % fails if done within an element
+    wf:wire(#api{ name=control_key, tag=[], delegate=element_outline }),
+    [
+	#flash{},
+	#panel{
+	    style="height:100%;margin-left:50px;margin-right:50px;margin-top:0px;",
+	    body=
+	    #table{
+		style="height:100%;width:100%",
+		rows=[
+		    #tablerow{
+			cells=[
+			    #tablecell{
+				class="resizable",
+				style="borders-right:1px solid;vertical-align:top",
+				body=
+				#panel{
+				    style="margin-top:0px",
+				    body=OutlineBody
+				}
+			    },
+			    #tablecell{
+				style="height:100%;padding:2px 2px 10px 2px;",
+				class="resizable",    
+				body=[
+				    #textarea{
+					id="textarea",
+					style="height:99%;width:99%;display:table-cell;",
+					text=binary_to_list(Text)},
+				    #hidden{
+					id="current_node", text=binary_to_list(FirstNode)}
+				]
 			    }
-			},
-			#tablecell{
-			    style="height:100%;border:1px solid;padding:2px 2px 10px 2px;",
-			    class="resizable",    
-			    body=#textarea{
-				id="textarea",
-				style="height:99%;width:99%;display:table-cell;",
-				text=Text}
-			}
-		]}
-	    ]
-	}
-    }.
+		    ]}
+		]
+	    }
+    }].
 
 vw_element_event(Other) ->
     wf:update(placeholder, Other),
     ?PRINT({vw_element_event, Other}).
 
+event(blur_text) ->
+    Key = list_to_binary(wf:q(current_node)),
+    Text = list_to_binary(wf:q(textarea)),
+    #graph_node{ title = Title,
+	text = OldText,
+	children = ChildList } = visiweave_node_server:read_node(Key),
+    case Text of
+	OldText -> ok;
+	_ ->
+	    visiweave_node_server:write_node(#graph_node{
+		key = Key,
+		title = Title,
+		text = Text,
+		children = ChildList})
+    end;
+    
 event(Other) ->
     wf:update(placeholder, Other),
     ?PRINT({event, Other}).
 
-handle_click({arrow, ArrowID}) ->
-    Arrow = wf:q(ArrowID),
-    ?PRINT({handle_click, ArrowID, Arrow}).
-
-% translate from visiweave_model record to outline element expected tuple
+% translate from visiweave_model record with binary members to outline element expected tuple with list members
 read_node(Id) ->
     #graph_node{ title = Title,
 	text = Text,
-	children = Childlist } = visiweave_node_server:read_node(list_to_binary(Id)),
-    { Title, Text, [binary_to_list(Child) || Child <- Childlist] }.
+	children = ChildList } = visiweave_node_server:read_node(list_to_binary(Id)),
+    {
+	binary_to_list(Title),
+	binary_to_list(Text),
+	[binary_to_list(Child) || Child <- ChildList]
+    }.
+
+write_node(Key, Title, Text, ChildList) ->
+    visiweave_node_server:write_node(#graph_node{
+	key = list_to_binary(Key),
+	title = list_to_binary(Title),
+	text = list_to_binary(Text),
+	children = [list_to_binary(Child) || Child <- ChildList]}).
